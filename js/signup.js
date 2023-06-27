eb8f149098f526b5ef968c39b534463d43f27c7f
DestinyArielLee <aryullee@gmail.com>
2023-06-26 21:51:00 -0700

    editing sign up page

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,23 @@
 'use strict'
+
+function createNewUser() {
+    const baseURL = "https://microbloglite.herokuapp.com/api/users";
+    const username = document.getElementById("username").value;
+    const fullname = document.getElementById("fullname").value;
+    const password = document.getElementById("password").value;
+    
+    const options = {
+        method: "POST",
+        headers: {
+         "Content-Type": "application/json"},
+        body: JSON.stringify({
+            "username": username,
+            "fullName": fullname,
+            "password": password
+        })
+    }
+
+fetch(baseURL, options) 
+    .then(response => response.json())
+    .then(window.location.href="signin.html")
+}
