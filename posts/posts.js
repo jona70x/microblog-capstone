d06502cc204421282de3665268182cdb8369c22b
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2023-06-27 16:06:19 -0700

    adds delete functionality

--- posts/posts.js
+++ posts/posts.js
@@ -94,6 +94,26 @@ const addLike = async function (postId) {
   return response;
 };
 
+const deletePost = async (postId) => {
+  const postsContainer = document.querySelector(".posts-container");
+  const postToDelete = document.querySelector(`[data-post-id="${postId}"]`);
+
+  console.log(postsContainer, postToDelete);
+  const response = await fetch(apiBaseURL + "/api/posts/" + postId, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ postId }),
+  });
+
+  console.log(response);
+  if (response.ok) {
+   postToDelete.remove()
+  }
+};
+
 const createPostsMarkup = async () => {
   const posts = await getPosts();
 
@@ -175,21 +195,23 @@ const createPostsMarkup = async () => {
           </div>
         </div>
         <div
-          class="svg-container svg-container__delete rounded border rounded-circle d-flex align-items-center justify-content-center"
+          class="svg-container svg-container__delete rounded border rounded-circle d-flex align-items-center justify-content-center delete-functionality"
         >
-          <div>
+          <div class='delete-functionality'>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-trash"
+              class="bi bi-trash delete-functionality"
               viewBox="0 0 16 16"
             >
               <path
+              class='delete-functionality'
                 d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"
               />
               <path
+              class='delete-functionality'
                 d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"
               />
             </svg>
@@ -202,10 +224,18 @@ const createPostsMarkup = async () => {
 
     generatedPost.addEventListener("click", async (e) => {
       const targetContainer = e.target;
+
       if (targetContainer.classList.contains("like-functionality")) {
         const postBox = targetContainer.closest("#post-box");
         const postId = postBox.dataset.postId;
         addLike(postId);
+      }
+
+      if (targetContainer.classList.contains("delete-functionality")) {
+        const postBox = targetContainer.closest("#post-box");
+        const postId = postBox.dataset.postId;
+        console.log("deleting");
+        deletePost(postId);
       }
     });
     postsContainer.appendChild(generatedPost);
