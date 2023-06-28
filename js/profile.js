5b502f2af490ad9b2fb7adb5e2eb91a556be9d5a
Sammi Chen <sammic432@users.noreply.github.com>
2023-06-28 15:59:05 -0700

    profile test page

--- js/profile.js
+++ js/profile.js
@@ -13,5 +13,12 @@ function redirectHome() {
   window.location.assign("/posts/");
 }
   
+let profileName = document.querySelector(".profile-name");
+function displayUsername(){
+  profileName.innerHTML = `<h2>${username}</h2>`;
+}
+
+displayUsername();
   //reload
   //logout/home buttons
+  //posts count
