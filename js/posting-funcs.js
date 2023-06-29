74e8ae5e07195fee70403a3dae4fe1cc94b92f8f
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2023-06-29 10:22:10 -0700

    add sorting functionality

--- js/posting-funcs.js
+++ js/posting-funcs.js
@@ -27,8 +27,8 @@ const sendPost = async (inputElement) => {
 // Generate posts
 
 const createPostsMarkup = async (postsArray) => {
-  postsContainer.innerHTML = "";
-  console.log(postsArray);
+
+  
   for (const post of postsArray) {
     let likes = post.likes.length;
     const isLiked = !!post.likes.find((like) => like.username === username);
@@ -332,8 +332,9 @@ async function generatePostForm(formContainer) {
           ) {
             postsContainer.innerHTML = "";
             const posts = await getPosts();
-            if (posts) {
-              createPostsMarkup(posts);
+            const sortedPosts =  sortByLatestPosts(posts)
+            if (sortedPosts) {
+              createPostsMarkup(sortedPosts);
             }
           } else if (
             responseInfo.ok &&
